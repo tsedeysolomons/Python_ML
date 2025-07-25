259ce9059de7dc10aaafb4c0e7c7b756295dcb1e
tsedey <167250386+tsedeysolomons@users.noreply.github.com>
2025-07-25 15:41:14 +0300

    Implement regression metrics (MAE, MSE, RMSE, R²) using sklearn for model performance evaluation

--- Supervised_Learning.py
+++ Supervised_Learning.py
@@ -5,6 +5,8 @@ from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score, a
 # Actual and predicted values
 y_true = [100, 200, 300,400,578,150]
 y_pred = [110, 180, 290,213,789, 458]
+x_true = [500,780,250,489]
+x_pred = [200,781,350,689]
 
 # Calculate metrics
 mae = mean_absolute_error(y_true, y_pred)
@@ -12,6 +14,7 @@ mse = mean_squared_error(y_true, y_pred)
 rmse = np.sqrt(mse)
 r2 = r2_score(y_true, y_pred)
 actual = accuracy_score(y_true, y_pred)
+f1 = f1_score(x_true, x_pred, average='macro')
 
 
 print("This is the test of Regeneration and Classification type test \n ")
@@ -22,3 +25,5 @@ print("R² Score = ", r2)
 
 # classification
 print("Accuracy = " , actual)
+print("f1_Score = ", f1)
+
